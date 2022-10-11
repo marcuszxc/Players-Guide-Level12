3213d7bf6b0d381c0667a480b36adaafabaf1f6b
marcu <marcu@MSI>
2022-10-11 13:30:03 +0200

    Add project files.

---
+++ TheLawsOfFreach/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TheLawsOfFreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] NumArray = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+            double Avg = 0;
+            int Min = 0;
+
+            foreach (int i in NumArray)
+            {
+                Avg += i;
+
+                if (Min > i)
+                {
+                    Min = i;
+                }
+            }
+
+            Console.WriteLine($"The average of the array is {Avg / NumArray.Length} and the smallest number in the array is {Min}.");
+
+        }
+    }
+}
---
+++ TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TheReplicatorOfDTo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] OrgArray = new int[5];
+            int[] CopyArray = new int[5];
+
+            for (int x = 0; x < OrgArray.Length; x++)
+            {   
+                Console.Write("Please enter a number: ");
+                OrgArray[x] = int.Parse(Console.ReadLine());
+            }
+
+            for (int z = 0; z < CopyArray.Length; z++)
+            {
+                CopyArray[z] = OrgArray[z];
+            }
+
+            for(int y = 0; y < OrgArray.Length; y++)
+            {
+                Console.WriteLine($"Original array: {OrgArray[y]} Copied array: {CopyArray[y]}");
+            }
+
+        }
+    }
+}
